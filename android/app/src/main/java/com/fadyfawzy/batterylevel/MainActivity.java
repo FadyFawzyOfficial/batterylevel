14006f1946b34988b1f24ea9af0677adeddff23b
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-19 20:10:41 +0300

    2. Add the Android Java code that uses the Android battery APIs to retrieve the battery level

--- android/app/src/main/java/com/fadyfawzy/batterylevel/MainActivity.java
+++ android/app/src/main/java/com/fadyfawzy/batterylevel/MainActivity.java
@@ -1,5 +1,12 @@
 package com.fadyfawzy.batterylevel;
 
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.android.FlutterActivity;
@@ -17,5 +24,24 @@ public class MainActivity extends FlutterActivity {
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler((call, result) -> {
             // This method is invoked in the main thread
         });
+    }
+
+    // Add the Android Java code that uses the Android battery APIs to retrieve the battery level.
+    // This code is exactly the same as you would write in a native Android app.
+    // Add the following as a new method in the activity class, below the configureFlutterEngine() method:
+    private int getBatteryLevel() {
+        int batteryLevel = -1;
+
+        if(VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP){
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+
+        return batteryLevel;
     }
 }
