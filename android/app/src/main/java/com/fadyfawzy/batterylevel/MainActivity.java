a8b4f9f30555342e27925badf749d19c00bad3f6
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-19 20:00:28 +0300

    1. Create a MethodChannel & Set a MethodCallHandler inside the configureFlutterEngine() method

--- android/app/src/main/java/com/fadyfawzy/batterylevel/MainActivity.java
+++ android/app/src/main/java/com/fadyfawzy/batterylevel/MainActivity.java
@@ -1,6 +1,21 @@
 package com.fadyfawzy.batterylevel;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    // Make sure to use the same channel name as was used on the Flutter client side.
+    private static final String CHANNEL = "fadyfawzy.com/battery";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler((call, result) -> {
+            // This method is invoked in the main thread
+        });
+    }
 }
